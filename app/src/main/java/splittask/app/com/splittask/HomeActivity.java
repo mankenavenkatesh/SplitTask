93c9bf498763ae21a403476387d48913e7a2cae2
Kusum Vanwani <Kusum_Vanwani@intuit.com>
2016-10-05 14:00:05 +0530

    changing names

--- app/src/main/java/splittask/app/com/splittask/HomeActivity.java
+++ app/src/main/java/splittask/app/com/splittask/HomeActivity.java
@@ -80,17 +80,15 @@ public class HomeActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.my_task) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.groups) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.contacts) {
 
-        } else if (id == R.id.nav_manage) {
+        }  else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.invite) {
 
         }
 
