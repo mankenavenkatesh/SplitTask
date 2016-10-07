bff3e9321de629e281684f7a93ce853886806cb4
Venkatesh Mankena <mankenavenkatesh@gmail.com>
2016-10-07 10:22:08 +0530

    Add Task Screen

--- app/src/main/java/splittask/app/com/splittask/TaskActivity.java
+++ app/src/main/java/splittask/app/com/splittask/TaskActivity.java
@@ -3,9 +3,17 @@ package splittask.app.com.splittask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class TaskActivity extends AppCompatActivity {
+public class TaskActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
 
     @Override
@@ -13,8 +21,45 @@ public class TaskActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
 
+        // Spinner element
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        // Spinner click listener
+        spinner.setOnItemSelectedListener(this);
+
+        // Spinner Drop down elements
+        List groups = new ArrayList();
+        groups.add("PCG-PD_India");
+        groups.add("Devopss");
+        groups.add("Lacerte");
+
+        // Creating adapter for spinner
+        ArrayAdapter dataAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, groups);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
     }
+
+    @Override
+    public void onItemSelected(AdapterView parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+
+    }
+
+    public void onNothingSelected(AdapterView arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
 }
